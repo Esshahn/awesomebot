1748474127e0fdc0bc3c814dced64906b4b4e53e
Ingo Hinterding <ingo@hinterding.de>
2023-03-26 12:34:28 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
-from os import system
+import os
 import sounddevice as sd
 from scipy.io.wavfile import write
+from dotenv import load_dotenv
 
-system("clear")
 
-# RECORD AUDIO
+def main():
+    os.system("clear")
+    load_dotenv()
 
-print("Hallo ich bin der Awesomebot vom CityLAB Berlin!")
+    # RECORD AUDIO
 
-fs = 22050  # Sample rate
-seconds = 6  # Duration of recording
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-print("\nBegin recording...")
-sd.wait()  # Wait until recording is finished
-print("End recording")
-write('output.wav', fs, myrecording)  # Save as WAV file
+    print("Hallo ich bin der Awesomebot vom CityLAB Berlin!")
 
-# TRANSCRIBE AUDIO VIA WHISPER
+    fs = 22050  # Sample rate
+    seconds = 6  # Duration of recording
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    print("\nBegin recording...")
+    sd.wait()  # Wait until recording is finished
+    print("End recording")
+    write('output.wav', fs, myrecording)  # Save as WAV file
 
-openai.api_key = "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"
+    # TRANSCRIBE AUDIO VIA WHISPER
 
-audio_file = open("output.wav", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print("Ich habe folgendes verstanden:")
-print(transcript.text)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    audio_file = open("output.wav", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print("Ich habe folgendes verstanden:")
+    print(transcript.text)
 
-# OUTPUT ANSWER FROM CHATGPT
+    # OUTPUT ANSWER FROM CHATGPT
 
-messages = []
-messages.append(
-    {"role": "system", "content": "The reply should be a transformation of the sentence into a very bureaucratic and formal language. keep it short but complex."})
+    messages = []
+    messages.append(
+        {"role": "system", "content": "Be precise, be concise, be polite and positive."})
 
-message = transcript.text
-messages.append({"role": "user", "content": message})
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages)
-reply = response["choices"][0]["message"]["content"]
-messages.append({"role": "assistant", "content": reply})
-print("\n" + reply + "\n")
-system("say -r180 -v 'Yannick' '" + reply+"'")
+    message = transcript.text
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
+    os.system("say -r180 " + reply)
+
+
+if __name__ == '__main__':
+    main()
