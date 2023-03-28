14b359564190b687440270e860818d4d37f5d97b
Ingo Hinterding <ingo@hinterding.de>
2023-03-28 12:53:54 +0200

    cleanup

--- main.py
+++ main.py
@@ -6,37 +6,30 @@ from scipy.io.wavfile import write
 from dotenv import load_dotenv
 
 
-def main():
-    os.system("clear")
-    load_dotenv()
-
-    # RECORD AUDIO
-
-    print("Hallo ich bin der Awesomebot vom CityLAB Berlin!")
-
+def record_audio(filename="output.wav"):
     fs = 22050  # Sample rate
     seconds = 6  # Duration of recording
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
     print("\nBegin recording...")
     sd.wait()  # Wait until recording is finished
     print("End recording")
-    write('output.wav', fs, myrecording)  # Save as WAV file
+    write(filename, fs, myrecording)  # Save as WAV file
 
-    # TRANSCRIBE AUDIO VIA WHISPER
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    audio_file = open("output.wav", "rb")
+def transcribe_audio(filename="output.wav"):
+    audio_file = open(filename, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print("Ich habe folgendes verstanden:")
     print(transcript.text)
+    return transcript.text
 
-    # OUTPUT ANSWER FROM CHATGPT
 
+def query_chatgpt(prompt):
     messages = []
     messages.append(
         {"role": "system", "content": "Be precise, be concise, be polite and positive."})
 
-    message = transcript.text
+    message = prompt
     messages.append({"role": "user", "content": message})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -44,6 +37,21 @@ def main():
     reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": reply})
     print("\n" + reply + "\n")
+    return reply
+
+
+def main():
+    os.system("clear")
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    soundfile_name = "output.wav"
+
+    print("Hallo ich bin der Awesomebot vom CityLAB Berlin!")
+
+    record_audio(soundfile_name)
+    prompt = transcribe_audio(soundfile_name)
+    reply = query_chatgpt(prompt)
+
     os.system("say -r180 " + reply)
 
 
